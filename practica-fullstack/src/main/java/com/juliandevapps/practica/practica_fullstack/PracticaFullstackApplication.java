d35e10c2f1ca99b44fe9d427865e4e4ace041c22
Julian Salinas <l20091180@zacatepec.tecnm.mx>
2026-02-05 20:33:46 -0600

    Initial commit (creación del proyecto)

---
+++ practica-fullstack/src/main/java/com/juliandevapps/practica/practica_fullstack/PracticaFullstackApplication.java
@@ -0,0 +1,13 @@
+package com.juliandevapps.practica.practica_fullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticaFullstackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticaFullstackApplication.class, args);
+	}
+
+}
---
+++ practica-fullstack/src/test/java/com/juliandevapps/practica/practica_fullstack/PracticaFullstackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juliandevapps.practica.practica_fullstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticaFullstackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
